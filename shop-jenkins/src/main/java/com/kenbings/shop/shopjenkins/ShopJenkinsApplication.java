3f33116612d3be2180bf16ecc624ca86a087b3c7
kenbings <kenbingthoughts@163.com>
2022-12-14 14:47:57 +0800

    使用Jenkins+Gitlab一键打包部署SpringBoot应用

---
+++ shop-jenkins/src/main/java/com/kenbings/shop/shopjenkins/ShopJenkinsApplication.java
@@ -0,0 +1,11 @@
+package com.kenbings.shop.shopjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopJenkinsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopJenkinsApplication.class, args);
+    }
+}
---
+++ shop-jenkins/src/test/java/com/kenbings/shop/shopjenkins/ShopJenkinsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kenbings.shop.shopjenkins;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJenkinsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
